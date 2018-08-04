57e8501d1c871c7d010fc961ee62608cc3105d78
Renan Passos <renanpassos@poli.ufrj.br>
2018-08-04 02:45:24 -0300

    Módulo extrator exemplificando o que é necessário para funcionar.

--- extrator.py
+++ extrator.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 # Módulo Python para extração de informações de um computador
 # Necessita de privilégios de super-uduário!
 # Use python 3.x
 
+
 import lib.xmltodict as xmltodict
 import os
+
 
 # Obtenção de informações de hardware pelo lshw:
 
@@ -24,12 +27,35 @@ lshw_dict = xmltodict.parse(texto) # lshw em forma de dicionário!!
 # Obtenção do log do kernel pelo dmesg:
 
 print ("Obtendo dmesg... ")
+
 status_comando = os.system("dmesg > dmesg.txt")
 if (status_comando == 0):
     print ("OK!")
 else:
     print ("Erro ao obter dmesg: " + str(status_comando))
 
+HWinfo = {
+	"Motherboard" : {} ,
+	"CPU" : {} ,
+	"RAM" : {} ,
+	"Storage" : {} ,
+	"Driver" : {}
+}
 
-print (lshw_dict["list"]["node"]["product"])
-print (lshw_dict["list"]["node"]["width"])
+HWinfo["CPU"]["Width"] = lshw_dict["list"]["node"]["width"]
+
+HWinfo_XML = xmltodict.unparse({"List" : HWinfo})
+
+print ("Gravando arquivo de informações... ")
+try:
+	HWinfo_file = open("18.01.001.xml", 'w')
+	HWinfo_file.write(HWinfo_XML + "\n")
+	HWinfo_file.close()
+	print ("OK!")
+except:
+    print ("Erro ao gravar arquivo.")
+
+
+#print (HWinfo)  # Visualiza o dicionário final.
+#print (HWinfo_XML)  # Visualizao xml que vai para o arquivo.
+#print (xmltodict.parse(HWinfo_XML))  # Visualiza o dicionário que seria obtido deste XML pelo módulo central.
--- labinfo.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-from ./lib/xmltodict.py import parse
-from ./lib/xmltodict.py import unparse
-
-
