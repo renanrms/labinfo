b50e92e37bb2c80704ec152c0f0344d19f4c4b32
Renan Passos <renanpassos@poli.ufrj.br>
2018-08-04 22:34:14 -0300

    Várias implementações e descrição das tarefas nos comentários.

--- extrator.py
+++ extrator.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Módulo Python para extração de informações de um computador
-# Necessita de privilégios de super-uduário!
+# Programa para extração de informações sobre hardware e software de um computador.
+# Necessita de privilégios de super-usuário!
 # Use python 3.x
 
 
@@ -10,52 +10,111 @@ import lib.xmltodict as xmltodict
 import os
 
 
-# Obtenção de informações de hardware pelo lshw:
+# Verifica se está com privilégios de super-usuário:
+# 	Ver se é possível pedir a senha e obter os privilégios aqui.
+
+# status_comando = os.system("sudo --validate")
+
+if (os.geteuid() != 0):
+	print ("AVISO: você deveria executar este programa como super-usuário.")
+	print ("       Sem privilégios de super-usuário algumas informações não podem ser acessadas.")
+	print ("")
+	print ("USO: sudo python3 extrator.py")
+	print ("OU   su")
+	print ("     python3 extrator.py")
+	print ("")
+
+# Solicita dados ao usuário:
+
+## Solicita o código de registro do computador:
+
+### INSERIR CÓDIGO ###
+
+## Solicita a pasta do laboratório onde registrar o computador:
+
+### INSERIR CÓDIGO ###
+
+## Confirma data e hora atuais:
+
+### INSERIR CÓDIGO ###
+
+# Obtém informações de hardware pelo lshw:
 
 print ("Obtendo lshw... ")
-status_comando = os.system("lshw -xml > lshw.xml")
+### Aqui vai um pouco de Shell Script:
+### redirecionaremos a saída do comando para "lshw.xml" e a saída de erro para ".lshw_error_messages".
+### redirecionar o erro é necessário para determinar quais mensagens aparecem para o usuário.
+status_comando = os.system("lshw -xml > lshw.xml 2> .lshw_error_messages")
 if (status_comando == 0):
     print ("OK!")
 else:
     print ("Erro ao obter lshw: " + str(status_comando))
+
+### Remove o arquivo temporário.
+os.remove(".lshw_error_messages")
 
 lshw_xml = open('lshw.xml', 'r')
 texto = lshw_xml.read()
 lshw_xml.close()
 lshw_dict = xmltodict.parse(texto) # lshw em forma de dicionário!!
 
-# Obtenção do log do kernel pelo dmesg:
+# Obtém o log do kernel pelo dmesg:
 
-print ("Obtendo dmesg... ")
+print ("Obtendo o log do kernel... ")
 
-status_comando = os.system("dmesg > dmesg.txt")
+status_comando = os.system("dmesg > dmesg.txt")  # Verificar a possibilidade de gravar a cor de cada linha neste arquivo.
 if (status_comando == 0):
     print ("OK!")
 else:
     print ("Erro ao obter dmesg: " + str(status_comando))
 
+# Agora inicializamos o dicionário e atribuímos os dados:
+
 HWinfo = {
-	"Motherboard" : {} ,
+	"Placa-mae" : {} ,
 	"CPU" : {} ,
 	"RAM" : {} ,
-	"Storage" : {} ,
-	"Driver" : {}
+	"Armazenamento" : {} ,
+	"Driver optico" : {} ,
+	"Estado" : {} ,
+	"Software" : {}
 }
 
-HWinfo["CPU"]["Width"] = lshw_dict["list"]["node"]["width"]
+## Obtém os dados do hardware:
 
-HWinfo_XML = xmltodict.unparse({"List" : HWinfo})
+HWinfo["CPU"]["largura"] = lshw_dict["list"]["node"]["width"]
 
-print ("Gravando arquivo de informações... ")
+### COMPLETAR CÓDIGO ###
+
+## Obtém os dados de saúde do hardware:
+
+### INSERIR CÓDIGO ###
+
+## Obtém os dados sobre programas instalados:
+
+### INSERIR CÓDIGO ###
+
+# Transforma o XML em dicionário e grava em arquivo:
+
+HWinfo = {"List" : HWinfo}  # O método exige que haja um nó raiz no dicionário.
+HWinfo_XML = xmltodict.unparse(HWinfo)
+
+print ("Gravando informações em arquivo... ")
 try:
 	HWinfo_file = open("18.01.001.xml", 'w')
 	HWinfo_file.write(HWinfo_XML + "\n")
 	HWinfo_file.close()
 	print ("OK!")
-except:
-    print ("Erro ao gravar arquivo.")
+except Exception as e:
+    print ("Erro: " + str(e))
+
+# Posiciona os arquivos e altera as permissões para que usuários possam mover os arquivos (chmod 644).
+
+### INSERIR CÓDIGO ###
 
 
+
+# Testes
 #print (HWinfo)  # Visualiza o dicionário final.
 #print (HWinfo_XML)  # Visualizao xml que vai para o arquivo.
 #print (xmltodict.parse(HWinfo_XML))  # Visualiza o dicionário que seria obtido deste XML pelo módulo central.
